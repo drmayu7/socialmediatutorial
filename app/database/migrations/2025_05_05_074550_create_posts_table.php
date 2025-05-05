219438932ae01c7644c79d348f6951b9986f177a
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-05-05 16:37:40 +0800

    initial commit

---
+++ app/database/migrations/2025_05_05_074550_create_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->string('title',255);
+            $table->text('description');
+            $table->bigInteger('user_id')->unsigned(); # unsigned to make it positive
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade'); # foreign key constraint
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
